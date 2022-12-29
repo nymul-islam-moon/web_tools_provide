6704464464e0a730ab2b67b75f0d619c13716a50
Nymul Islam Moon <towkir1997islam@gmail.com>
2022-12-29 20:52:54 +0600

    Conflict Merged

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,27 +1,13 @@
 <!doctype html>
 <html lang="en">
-<<<<<<< HEAD
-<head>
-=======
   <head>
->>>>>>> 62c7084b71c0944af3d3490bf647bc839bed297a
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<<<<<<< HEAD
-</head>
-<body>
-
-@yield('content')
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</body>
-=======
   </head>
   <body>
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
->>>>>>> 62c7084b71c0944af3d3490bf647bc839bed297a
 </html>
